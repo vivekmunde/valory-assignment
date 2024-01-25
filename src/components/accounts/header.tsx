541436358f120fa378a9a090d5b4cb29075821e0
Vivek <vivekmunde@users.noreply.github.com>
2024-01-25 17:19:55 +0530

    Hide when loading

--- src/components/accounts/header.tsx
+++ src/components/accounts/header.tsx
@@ -2,21 +2,29 @@ import { ReloadOutlined } from "@ant-design/icons";
 import Button from "antd/lib/button";
 import Space from "antd/lib/space";
 import React from "react";
+import { useGetState } from "recell";
+import accountsCell from "../../cells/accounts";
 import useLoadAccounts from "../accounts-provider/use-load-accounts";
+import If from "../if";
 import ReloadingIcon from "./reloading-icon";
 
 const Header: React.FC = () => {
   const { onReload } = useLoadAccounts();
+  const loading = useGetState(accountsCell, (state) => state.loading);
 
   return (
-    <Space size="small" direction="horizontal">
-      <ReloadingIcon />
-      <Button
-        type="text"
-        icon={<ReloadOutlined />}
-        onClick={() => onReload()}
-      />
-    </Space>
+    <If condition={loading}>
+      <If.False>
+        <Space size="small" direction="horizontal">
+          <ReloadingIcon />
+          <Button
+            type="text"
+            icon={<ReloadOutlined />}
+            onClick={() => onReload()}
+          />
+        </Space>
+      </If.False>
+    </If>
   );
 };
 
